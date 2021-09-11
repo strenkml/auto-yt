717a5007c2516c4490c4af84b3261508d96db6c7
Matt Strenk <strenkml@users.noreply.github.com>
2021-09-11 05:12:49 -0400

    Start adding logic for the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Enmap = require("enmap");
 const cron = require("node-cron");
+const { spawn } = require("child_process");
 
 const config = require("./config.json");
 
@@ -7,12 +8,16 @@ const settings = new Enmap({
   name: "settings",
   autoEnsure: {
     cookiesFile: "cookies.txt",
+    timezone: "America/New_York",
   },
 });
 
 const sources = new Enmap({
   name: "sources",
 });
+
+// Declare variables
+var crons = [];
 
 init();
 
@@ -21,9 +26,11 @@ settings.changed(sourceChanged);
 
 function init() {
   var sourcesArray = sources.array();
-  sourcesArray.forEach((source) => {
+  var keysArray = sources.keysArray();
+
+  sourcesArray.forEach((source, index) => {
     if (source.cron != "no") {
-      createCron(source);
+      createCron(source, keysArray[index]);
     }
   });
 }
@@ -32,4 +39,57 @@ function sourceChanged(key, oldVal, newVal) {}
 
 function settingsChanged(key, oldVal, newVal) {}
 
-function createCron(source) {}
+function createCron(source, key) {
+  let task = cron.schedule(
+    source.cron,
+    () => {
+      var download = spawn(
+        createYTDLCommand(source.url, source.name, source.metadata)
+      );
+
+      download.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+      });
+
+      download.stderr.on("data", (data) => {
+        console.log(`stderr: ${data}`);
+      });
+    },
+    { scheduled: true, timezone: settings.get("timezone") }
+  );
+
+  crons.push({
+    key: key,
+    task: task,
+    running: true,
+  });
+}
+
+function removeCron(key) {
+  var _cron = crons.find((item) => item.key === key);
+  _cron.task.destroy();
+
+  // Remove the destroyed task from the crons array
+  crons = crons.find((item) => item.key != key);
+}
+
+function editCron(key, newSource) {
+  removeCron(key);
+  createCron(newSource, key);
+}
+
+function pauseCron(key) {
+  var _cron = crons.find((item) => item.key === key);
+  _cron.task.stop();
+  crons.map((item) => (item.running = false));
+}
+
+function resumeCron(key) {
+  var _cron = crons.find((item) => item.key === key);
+  _cron.task.start();
+  crons.map((item) => (item.running = true));
+}
+
+function createYTDLCommand(url, name, metadata) {
+  return `youtube-dl "${url}" --download-archive "${config.downloadDir}/${name}/downloaded.txt" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --output "${config.downloadDir}/${name}/${metadata}" --retries infinite --fragment-retries infinite --continue --no-overwrites --embed-thumbnail --embed-subs --add-metadata`;
+}
